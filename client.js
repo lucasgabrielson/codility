7500ba1761bfa95cacd97b82e93cac4b4dffd4c1
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-06-07 21:45:50 -0500

    shift problem

---
+++ client.js
@@ -0,0 +1,29 @@
+const shift = (A, K) => {
+    // write your code in JavaScript (Node.js 4.0.0)
+    
+    let result = [];
+    
+    if(A.length === 1 || K === 0) {
+        return A;
+    }
+    
+    for(let i = 0; i < A.length; i++) {
+        let newPos = ( i + K ) % A.length;
+        
+        result[newPos] = A[i];
+    }
+    
+    return result;
+}
+
+/**
+ * [1,2,3,4,5]
+ * K = 2
+ * newPos = ( 0 + 2 ) % 5 = 2
+ * newPos = ( 1 + 2 ) % 5 = 3
+ * newPos = ( 2 + 2 ) % 5 = 4
+ * newPos = ( 3 + 2 ) % 5 = 0
+ * newPos = ( 4 + 2 ) % 5 = 1
+ * 
+ * 
+ */
